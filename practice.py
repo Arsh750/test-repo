2ba6ada34d558928bdb1c5743d76feba32feb4ec
Arsh750 <kkrishnammalviya@gmail.com>
2024-10-14 18:00:37 +0530

    successfully commit

---
+++ practice.py
@@ -0,0 +1,36 @@
+# 101:50000
+# 102:45000
+# 103:55000
+# input from user and input accountId and amount, by adding sum 500 each and also update new account as 
+# 104:30000
+items={101:50000,102:45000,103:55000}
+# id=input("enter account id:")
+# amount=input("enter amount:")
+# items.update({id:amount})
+# print("New Sccount Created")
+# print(items)
+
+
+# print("Before adding '''500''' in items:",items)
+# #increment each value by 500
+# for id,amount in items.items():
+
+#     items[id] +=500
+# print("After adding '''500''' in items:",items)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+  
